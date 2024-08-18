f3b8a98bccc008c8fa779d537882cc241873eb59
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-08-18 20:18:38 +0900

    とりあえず

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod generator;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn print_random_number() {
+    let n = generator::gen_ran();
+    println!("Random u8: {}", n);
 }
